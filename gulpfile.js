9c1de88348c127735c7b790ec6dd40756f892af2
Sofyan Sitorus <sofyansitorus@gmail.com>
2018-01-24 02:15:45 +0700

    Update gulp: enable autoprefix

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
+var autoprefixer = require('gulp-autoprefixer');
 
 var scssSrc = 'assets/scss/*.scss';
 var scssDest = 'assets/css';
@@ -15,10 +16,25 @@ var jsSrc = 'assets/js/*.js';
 var jsSrcExclude = '!assets/js/*.min.js';
 var jsDest = 'assets/js';
 
+var AUTOPREFIXER_BROWSERS = [
+    'Android 2.3',
+    'Android >= 4',
+    'Chrome >= 35',
+    'Firefox >= 31',
+    'Explorer >= 9',
+    'iOS >= 7',
+    'Opera >= 12',
+    'Safari >= 7.1',
+];
+
 // Compiles SCSS files from /scss into /css
 gulp.task('sass', function () {
     return gulp.src(scssSrc)
         .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: AUTOPREFIXER_BROWSERS,
+            cascade: false
+        }))
         .pipe(gulp.dest(scssDest));
 });
 
