a73d0a2d4a54dc31f534fae1d451780f105ce7c5
Sofyan Sitorus <sofyansitorus@gmail.com>
2018-01-24 17:24:53 +0700

    Optimize live-preview.js

--- assets/js/live-preview.js
+++ assets/js/live-preview.js
@@ -23,64 +23,67 @@
 
 		_bindSettings: function () {
 			var self = this;
-			self._bindInjectText('global_flash_sale_text', $('.product span.onsale'));
-			self._bindInjectText('product_loop_add_to_cart_btn_text_simple', $('.product-type-simple .product_type_simple'));
-			self._bindInjectText('product_loop_add_to_cart_btn_text_variable', $('.product-type-variable .product_type_variable'));
-			self._bindInjectText('product_loop_add_to_cart_btn_text_grouped', $('.product-type-grouped .product_type_grouped'));
-			self._bindInjectText('product_single_add_to_cart_btn_text', $('.single-product .single_add_to_cart_button'));
-			self._bindToggleElement('cart_display_cross_sells', $('.cart-collaterals .cross-sells'));
-			self._bindCustomCallback('product_single_tabs', function (setting, settingId) {
+			self._injectText('global_flash_sale_text', $('.product span.onsale'));
+			self._injectText('product_loop_add_to_cart_btn_text_simple', $('.product-type-simple .product_type_simple'));
+			self._injectText('product_loop_add_to_cart_btn_text_variable', $('.product-type-variable .product_type_variable'));
+			self._injectText('product_loop_add_to_cart_btn_text_grouped', $('.product-type-grouped .product_type_grouped'));
+			self._injectText('product_single_add_to_cart_btn_text', $('.single-product .single_add_to_cart_button'));
+			self._toggleElement('woomizer_cart_display_cross_sells', $('.cart-collaterals .cross-sells'));
+			self._customCallback('product_single_tabs', function (setting, settingId) {
 				self.api.selectiveRefresh.bind('partial-content-rendered', function (placement) {
 					$('.wc-tabs-wrapper, .woocommerce-tabs, #rating').trigger('init');
 				});
 			});
 		},
 
-		_bindInjectText: function (settingId, $selector) {
+		_injectText: function (settingId, $selector) {
 			var self = this;
-			if (settingId.indexOf(woomizer_live_preview_params.prefix) !== 0) {
-				settingId = woomizer_live_preview_params.prefix + '_' + settingId;
-			}
+			settingId = self._autoPrefix(settingId);
 			self.api(settingId, function (setting) {
-				self._injectText(self.api.value(settingId)(), $selector);
+				$selector.text(self.api.value(settingId)());
 				setting.bind(function (newVal) {
-					self._injectText(newVal, $selector);
+					$selector.text(newVal);
 				});
 			});
 		},
 
-		_bindInjectAttr: function (settingId, $selector, attrName) {
+		_injectAttr: function (settingId, $selector, attrName) {
 			var self = this;
-			if (settingId.indexOf(woomizer_live_preview_params.prefix) !== 0) {
-				settingId = woomizer_live_preview_params.prefix + '_' + settingId;
-			}
+			settingId = self._autoPrefix(settingId);
 			self.api(settingId, function (setting) {
-				self._injectAttr(self.api.value(settingId)(), $selector, attrName);
+				$selector.attr(attrName, self.api.value(settingId)());
 				setting.bind(function (newVal) {
-					self._injectAttr(newVal, $selector, attrName);
-				});
-
-			});
-		},
-
-		_bindToggleElement: function (settingId, $selector) {
-			var self = this;
-			if (settingId.indexOf(woomizer_live_preview_params.prefix) !== 0) {
-				settingId = woomizer_live_preview_params.prefix + '_' + settingId;
-			}
-			self.api(settingId, function (setting) {
-				self._toggleElement(self.api.value(settingId)(), $selector);
-				setting.bind(function (newVal) {
-					self._toggleElement(newVal, $selector);
+					$selector.attr(attrName, newVal);
 				});
 			});
 		},
 
-		_bindCustomCallback: function (settingId, callback) {
+		_toggleElement: function (settingId, $selector) {
 			var self = this;
-			if (settingId.indexOf(woomizer_live_preview_params.prefix) !== 0) {
-				settingId = woomizer_live_preview_params.prefix + '_' + settingId;
+			settingId = self._autoPrefix(settingId);
+			self.api(settingId, function (setting) {
+				_toggleElement(self.api.value(settingId)(), $selector);
+				setting.bind(function (newVal) {
+					_toggleElement(newVal, $selector);
+				});
+			});
+
+			function _toggleElement(value, $selector) {
+				var isHidden = ["no", "none", "hidden"];
+				$selector.addClass('woomizer-toggle-preview');
+				if (isHidden.indexOf(value) !== -1) {
+					$selector.addClass('woomizer-hidden');
+					$selector.removeClass('woomizer-visible');
+				} else {
+					$selector.addClass('woomizer-visible');
+					$selector.removeClass('woomizer-hidden');
+				}
 			}
+		},
+
+		_customCallback: function (settingId, callback) {
+			var self = this;
+			settingId = self._autoPrefix(settingId);
 			if (typeof callback === "function") {
 				self.api(settingId, function (setting) {
 					callback(setting, settingId);
@@ -88,25 +91,13 @@
 			}
 		},
 
-		_injectText: function (value, $selector) {
-			$selector.text(value);
-		},
-
-		_injectAttr: function (value, $selector, attrName) {
-			$selector.attr(attrName, value);
-		},
-
-		_toggleElement: function (value, $selector) {
-			var isHidden = ["no", "none", "hidden"];
-			$selector.addClass('woomizer-toggle-preview');
-			if (isHidden.indexOf(value) !== -1) {
-				$selector.addClass('woomizer-hidden');
-				$selector.removeClass('woomizer-visible');
-			} else {
-				$selector.addClass('woomizer-visible');
-				$selector.removeClass('woomizer-hidden');
+		_autoPrefix: function (settingId) {
+			if (settingId.indexOf(woomizer_live_preview_params.prefix) !== 0) {
+				settingId = woomizer_live_preview_params.prefix + '_' + settingId;
 			}
-		}
+			return settingId;
+		},
+
 	};
 
 	$(document).ready(function () {
