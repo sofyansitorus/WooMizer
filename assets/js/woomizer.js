3cff358e12dd681b879ca9b5e8ac6c853c7d606b
Sofyan Sitorus <sofyansitorus@gmail.com>
2018-01-24 18:05:18 +0700

    Fix woomizer.js

--- assets/js/woomizer.js
+++ assets/js/woomizer.js
@@ -2,34 +2,35 @@
  * This file adds some LIVE to the Theme Customizer live preview.
  */
 (function ($) {
-    'use strict';
+	'use strict';
 
 	/**
 	 * wp.customize.woomizerFront
 	 *
 	 */
-    var woomizerFront = {
+	var woomizerFront = {
 
-        init: function () {
-            this._addToggles();
-        },
+		init: function () {
+			this._bindToggleElements();
+		},
 
-        _toggleElement: function (value, $selector) {
-            if (value == 'no' || value == 'none' || value == 'hidden') {
-                $selector.addClass('woomizer-toggle-hidden');
-            } else {
-                $selector.removeClass('woomizer-toggle-hidden');
-            }
-        },
+		_toggleElement: function (value, $selector) {
+			var isHidden = ["no", "none", "hidden"];
+			if (isHidden.indexOf(value) !== -1) {
+				$selector.addClass('woomizer-toggle-hidden');
+			} else {
+				$selector.removeClass('woomizer-toggle-hidden');
+			}
+		},
 
-        _addToggles: function () {
-            var self = this;
-            $.each(woomizer_params.toggle_elements, function (key, element) {
-                self._toggleElement(element.value, $(element.selector));
-            });
-        },
-    };
+		_bindToggleElements: function () {
+			var self = this;
+			$.each(woomizer_params.toggle_elements, function (key, element) {
+				self._toggleElement(element.value, $(element.selector));
+			});
+		},
+	};
 
-    woomizerFront.init(wp.customize);
+	woomizerFront.init();
 
 })(jQuery);
